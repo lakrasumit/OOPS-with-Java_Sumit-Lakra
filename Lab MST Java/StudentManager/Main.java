bfd5c65e0489802bb85a229940f3c2259b238bbd
Sumit Lakra <99547820+lakrasumit@users.noreply.github.com>
2026-03-11 23:40:51 +0530

    Delete Lab MST Java/StudentManager directory

--- Lab MST Java/StudentManager/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        StudentManager sm = new StudentManager();
-        sm.writeStudent();
-        sm.readStudents();
-    }
-}
